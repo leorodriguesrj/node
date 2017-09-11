e1d001868d00c33d1f0ce58aeffe9395703b04f4
Leonardo Rodrigues <leorodriguesrj@gmail.com>
2017-09-11 01:46:06 -0300

    Reverted to previous config
    - Host compilation uses 32bits
    - Using android API 24 on target
    - Using armeabi on target

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,4 +1,4 @@
 make distclean
 cp deps/icu4c-59_1-src.zip-old deps/icu4c-59_1-src.zip
-./android-configure-alternate $NDK_PATH arm64
+./android-configure-alternate $NDK_PATH
 ninja -vn -j1 -C out/Release > dry-run.txt
